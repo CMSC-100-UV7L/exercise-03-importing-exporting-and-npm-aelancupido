a4077a698d09fa424bdf51ede84d4d61712fdc0f
aelancupido <amcupido@up.edu.ph>
2023-03-10 12:45:37 +0800

    fix: changed faulty import statements to proper imports

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
-import isEmail from 'validator/lib/isEmail'
-import { v4 as uuidv4 } from 'uuid';
-
+const validator = require('validator');
+const { v4: uuidv4 } = require('uuid');
 
 function generateUniqueID(fName, lName){
     var id = "";
     id = id + fName[0].toLowerCase() + lName.toLowerCase();
-
+    var alphaNumbericVal = uuidv4().substring(0, 8);
+    id = id + alphaNumbericVal;
     return id;
 }
 
@@ -14,10 +14,22 @@ function addAccount(fName, lName, email, age){
         if(fName != "" && lName != "" && email != ""){
             if(validator.isEmail(email)){
                 if(age >= 18){
-                    //write txt file
+                    fs.appendFileSynch("users.txt", fName + ",");
+                    fs.appendFileSynch("users.txt", lName + ",");
+                    fs.appendFileSynch("users.txt", email + ",");
+                    fs.appendFileSynch("users.txt", age + ",");
+                    return true;
+                } else{
+                    return false;
                 }
+            } else{
+                return false;
             }
+        } else{
+            return false;
         }
+    } else{
+        return false;
     }
     
 }
